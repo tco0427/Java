741c4556f9807f12fcff9cc4b316dd22f80aaba6
DongKyu <a29661498@gmail.com>
2020-03-08 16:49:36 +0900

    Create CaffeineBeverage.java

---
+++ DesignPattern/TemplateMethodPattern/CaffeineBeverage.java
@@ -0,0 +1,14 @@
+package TemplateMethodPattern;
+
+public abstract class CaffeineBeverage {
+    final void prepareRecipe(){
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+    abstract void brew();
+    abstract void addCondiments();
+    void boilWater(){System.out.println("물 끓이는 중");}
+    void pourInCup(){System.out.println("컵에 따르는 중");}
+}
