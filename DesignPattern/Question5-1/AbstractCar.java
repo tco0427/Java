348be9be78f189eefdea6788b9faf44298cc8d43
DongKyu <a29661498@gmail.com>
2020-11-15 01:55:42 +0900

    Add files via upload
    옵션 모두 출력하도록 수정

--- DesignPattern/Question5-1/AbstractCar.java
+++ DesignPattern/Question5-1/AbstractCar.java
@@ -1,14 +1,15 @@
-//AbstractVehicle클래스를 상속받는 AbstractCar추상클래스와 AbstractVan추상클래스를 정의하였다.
-//이를 통해 탈 것(Vehicle)으로 추상화 되어 있던 것을 Van과 Car로 구체화, 세분화하였다.
 public abstract class AbstractCar extends AbstractVehicle {
-    //생성자를 오버로딩하여 색정보없이 엔진정보만을 가지고도 인스턴스 생성이 가능하게 하였다.(물론 AbstractCar를 대상으로 인스턴스생성하는 것잉 아닌 이를 구상하는 클래스를 대상으로 인스턴스를 생성하는 것이다.)
-    //이 때 두 생성자 모두에서 상위클래스의 생성자를 호출하고 있다.
     public AbstractCar(Engine engine) {
         super(engine);
+        description=getClass().getSimpleName();
     }
     public AbstractCar(Engine engine, Colour colour) {
         super(engine, colour);
+        description=getClass().getSimpleName();
     }
     @Override
     public abstract int getPrice();
+    public String getDescription(){
+        return description;
+    }
 }
--- DesignPattern/Question5-1/AbstractVehicleOption.java
+++ DesignPattern/Question5-1/AbstractVehicleOption.java
@@ -1,17 +1,14 @@
-//Vehicle를 구현한 구상클래스(차량)을 꾸며주는 클래스들에 대한 추상화를 추상클래스AbstractVehicleOption클래스를 통해 하였다.
-//데코레이터의 상위클래스는 자신이 꾸미고 있는 객체의 상위클래스와 동일해야한다. 따라서 AbstractVehicle을 상속하도록 하였다.
 public abstract class AbstractVehicleOption extends AbstractVehicle {
-    //이를 구현하는 클래스마다 모두 동일하게 탈것(Vehicle)을 꾸미기 때문에 상위의 추상클래스에서 선언해주었다.
-    //상속관계의 하위 클래스에서 접근 가능하도록 protected로 접근지정자를 설정해주었다.
     protected Vehicle decoratedVehicle;
 
-    //생성자에서 자신이 꾸미고 있는 객체에 대한 래퍼런스와 함께 자신이 꾸미고 있는 객체의 엔진과 색에 대한 정보를 함께 가지고 있는다.
-    //이렇게 한 이유는 출력시에 꾸미고 있는 객체에 대한 엔진과 색에 대한 정보도 함께 출력하고 있기 때문이다.
     public AbstractVehicleOption(Vehicle vehicle) {
-        super(vehicle.getEngine(), vehicle.getColour());
+        super(vehicle.getEngine(),vehicle.getColour());
         decoratedVehicle = vehicle;
     }
-
     @Override
     public abstract int getPrice();
+    @Override
+    public String getDescription(){
+        return getClass().getSimpleName()+","+decoratedVehicle.getDescription();
+    }
 }
